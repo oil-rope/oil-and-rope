bff6e036df15ddce25542a612dfb2f0f01698e37
Daniel <danielpaz4c@gmail.com>
2019-09-25 08:31:12 +0000

    test

--- oilandrope/bot/urls.py
+++ oilandrope/bot/urls.py
@@ -2,6 +2,9 @@ from django.urls import include, path
 from rest_framework import routers
 from . import viewsets
 
+
+# Test
+
 router = routers.DefaultRouter()
 router.register(r'discord_user', viewsets.DiscordUserViewSet)
 router.register(r'discord_server', viewsets.DiscordServerViewSet)
