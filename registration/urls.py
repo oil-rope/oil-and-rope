32698822c01ad73ad5e4b24501acb3b9e93ab47f
LeCuay <suso.becerra98@gmail.com>
2020-03-09 13:58:15 +0100

    Fixed isort

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from . import viewsets, views
+from . import views, viewsets
 
 router = routers.DefaultRouter()
 router.register(r'user', viewsets.UserViewSet)
