1e61d3faac6984e999cb9d2e6858477db6cb87d8
LeCuay <suso.becerra98@gmail.com>
2020-03-15 12:51:33 +0100

    SignUp view tests

--- tests/registration/test_views.py
+++ tests/registration/test_views.py
@@ -1,6 +1,11 @@
-from django.contrib.auth import get_user_model
+from unittest import mock
+
+from django.core import mail
+from django.contrib.auth import get_user, get_user_model
 from django.shortcuts import reverse
 from django.test import TestCase
+from django.utils.translation import ugettext_lazy as _
+from faker import Faker
 from model_bakery import baker
 
 
@@ -9,15 +14,33 @@ class TestLoginView(TestCase):
     Checks LoginView works correctly.
     """
 
+    def setUp(self):
+        self.faker = Faker()
+        self.user = baker.make(get_user_model())
+        self.url = reverse('registration:login')
+
     def test_access_ok(self):
-        response = self.client.get(reverse('registration:login'))
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200, 'User cannot access view.')
         self.assertTemplateUsed(response, 'registration/login.html')
 
-    def test_cannot_access_login_page_when_logged(self):
-        user = baker.make(get_user_model())
-        self.client.force_login(user)
-        response = self.client.get(reverse('registration:login'))
+    def test_user_can_login_ok(self):
+        # Change password so we can control input
+        password = ''.join(self.faker.words(4))
+        self.user.set_password(password)
+        self.user.save()
+        data = {
+            'username': self.user.username,
+            'password': password
+        }
+
+        self.assertFalse(get_user(self.client).is_authenticated, 'User is logged before post to Login.')
+        self.client.post(self.url, data=data)
+        self.assertTrue(get_user(self.client).is_authenticated, 'User is not logged.')
+
+    def test_cannot_access_if_user_is_logged(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 302, 'User is not redirected.')
 
 
@@ -26,7 +49,75 @@ class TestSignUpView(TestCase):
     Checks if SignUpview works correctly.
     """
 
+    def setUp(self):
+        self.faker = Faker()
+        profile = self.faker.simple_profile()
+        email = self.faker.safe_email()
+        password = ''.join(self.faker.words(3))
+        self.data_ok = {
+            'username': profile['username'],
+            'email': email,
+            'password1': password,
+            'password2': password
+        }
+        self.url = reverse('registration:register')
+
     def test_access_ok(self):
-        response = self.client.get(reverse('registration:register'))
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200, 'User cannot access view.')
         self.assertTemplateUsed(response, 'registration/register.html')
+
+    @mock.patch('registration.views.messages')
+    def test_user_can_register_ok(self, mock_call: mock.MagicMock):
+        response = self.client.post(self.url, data=self.data_ok)
+        succes_message = '{} {}.'.format(
+            _('User created!'),
+            _('Please confirm your email')
+        )
+        mock_call.success.assert_called_with(
+            response.wsgi_request,
+            succes_message
+        )
+
+    def test_user_is_created_ok(self):
+        self.client.post(self.url, data=self.data_ok)
+        user_exists = get_user_model().objects.filter(username=self.data_ok['username']).exists()
+        self.assertTrue(user_exists, 'User is not created.')
+
+    def test_email_sent_ok(self):
+        with self.settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend'):
+            self.client.post(self.url, data=self.data_ok, follow=True)
+            self.assertTrue(len(mail.outbox) == 1, 'Email wasn\'t sent.')
+
+    def test_wrong_confim_password(self):
+        data_ko = self.data_ok.copy()
+        data_ko['password2'] = self.faker.word()
+        response = self.client.post(self.url, data=data_ko)
+        self.assertFormError(response, 'form', 'password2', 'The two password fields didn\'t match.')
+
+    def test_required_fields_not_given(self):
+        data_without_email = self.data_ok.copy()
+        del data_without_email['email']
+        response = self.client.post(self.url, data=data_without_email)
+        self.assertFormError(response, 'form', 'email', 'This field is required.')
+
+        data_without_username = self.data_ok.copy()
+        del data_without_username['username']
+        response = self.client.post(self.url, data=data_without_username)
+        self.assertFormError(response, 'form', 'username', 'This field is required.')
+
+        data_without_password1 = self.data_ok.copy()
+        del data_without_password1['password1']
+        response = self.client.post(self.url, data=data_without_password1)
+        self.assertFormError(response, 'form', 'password1', 'This field is required.')
+
+        data_without_password2 = self.data_ok.copy()
+        del data_without_password2['password2']
+        response = self.client.post(self.url, data=data_without_password2)
+        self.assertFormError(response, 'form', 'password2', 'This field is required.')
+
+    def test_cannot_access_if_user_is_logged(self):
+        user = baker.make(get_user_model())
+        self.client.force_login(user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302, 'User is not redirected.')
