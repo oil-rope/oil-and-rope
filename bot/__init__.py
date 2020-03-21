9e955f4687a632febfb4cd335109edbe85a443a1
LeCuay <suso.becerra98@gmail.com>
2020-03-21 17:41:19 +0100

    I think we got the correct approach

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,20 +6,75 @@ A bot connected to Oil & Rope web to make Roleplay Games more interactive.
 """
 
 import logging
+import os
+import pathlib
 from datetime import datetime
 
 import discord
 from discord.ext import commands
+from django.utils.translation import ugettext_lazy as _
+from dotenv import load_dotenv
+
+from .exceptions import OilAndRopeException, HelpfulError
 
 LOGGER = logging.getLogger(__name__)
 
 
 class OilAndRopeBot(commands.Bot):
     """
+    Custom class to control the behaviour of the bot by enviroment variables.
     """
 
-    def __init__(self, *args, **kwargs):
-        super(OilAndRopeBot, self).__init__(*args, **kwargs)
+    def __init__(self, env_file=None, **options):
+        if env_file:
+            self.load_env_file(env_file)
+        self.load_variables()
+        super(OilAndRopeBot, self).__init__(command_prefix=self.command_prefix, description=self.description, **options)
+        self.load_commands()
+
+    def load_variables(self):
+        """
+        Loads required variables for the bot.
+        """
+
+        try:
+            self.command_prefix = os.environ['BOT_COMMAND_PREFIX']
+            self.description = os.getenv('BOT_DESCRIPTION', '')
+            self.token = os.environ['BOT_TOKEN']
+        except KeyError as ex:
+            missing_variables = ', '.join(ex.args)
+            error_message = 'Environment variables %s not found.' % missing_variables
+            solution = 'Use .env file or set up %s environment variables.' % missing_variables
+            raise HelpfulError(error_message, solution, preface='Error loading bot variables.')
+
+    def load_env_file(self, env_file: pathlib.Path):
+        """
+        Looks for the given .env file and sets up Environment Variables.
+        """
+
+        if not env_file.exists():
+            raise OilAndRopeException(_('Env file doesn\'t exist.'))
+        load_dotenv(env_file.as_posix())
+
+    def load_commands(self):
+        """
+        Reads all the commands from `bot.commands` and adds them to the bot command list.
+        """
+
+        print('\nLoading commands ', end='')
+
+        from bot.commands import logout
+
+        commands = [logout]
+        commands_to_load = [c.name for c in commands]
+        LOGGER.info('Commands to load: %s', ', '.join(commands_to_load))
+
+        for command in commands:
+            self.add_command(command)
+            print('.', end='')
+
+        # Linebreak to clean up
+        print('\n')
 
     async def on_ready(self):
         init_message = '{bot} is ready!\nID: {id}\nAt {time}'.format(
@@ -27,6 +82,7 @@ class OilAndRopeBot(commands.Bot):
             id=self.user.id,
             time=datetime.now().strftime('%d/%m/%Y %H:%M')
         )
+        LOGGER.info(init_message)
         print(init_message)
 
     async def on_message(self, message: discord.Message):
@@ -37,3 +93,6 @@ class OilAndRopeBot(commands.Bot):
         )
         print(log_message)
         await super(OilAndRopeBot, self).on_message(message)
+
+    def run(self, *args, **kwargs):
+        super(OilAndRopeBot, self).run(self.token, *args, **kwargs)
--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -1,24 +1,21 @@
-import os
 import pathlib
 
-from discord.ext import commands
-from django.core.management.base import BaseCommand, CommandError, CommandParser
-from django.utils.translation import ugettext_lazy as _
-from dotenv import load_dotenv
+from django.core.management.base import BaseCommand, CommandParser
 
 from bot import OilAndRopeBot
+from bot.exceptions import OilAndRopeException, HelpfulError
 
 
 class Command(BaseCommand):
 
-    help = _('Runs Discord Bot')
+    help = 'Runs Discord Bot.'
 
     def setup(self, env_file=None):
         self.bot = OilAndRopeBot(env_file=env_file)
         self.bot.run()
 
     def add_arguments(self, parser: CommandParser):
-        parser.add_argument('--env', nargs='?', type=str, required=False, help=_('Custom .env file to read.'))
+        parser.add_argument('--env', nargs='?', type=str, required=False, help='Custom .env file to read.')
 
     def handle(self, *args, **options):
         env_file = options['env']
@@ -29,6 +26,6 @@ class Command(BaseCommand):
                 env_file = pathlib.Path(env_file)
             # Start the bot
             self.setup(env_file)
-        except CommandError as ex:
+        except (OilAndRopeException, HelpfulError) as ex:
             message_error = '{error}'.format(error=ex)
             self.stdout.write(self.style.ERROR(message_error))
