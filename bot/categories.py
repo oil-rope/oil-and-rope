406ad88a6d49456c14791511a8d14abdb75227f6
LeCuay <suso.becerra98@gmail.com>
2020-03-22 13:31:12 +0100

    Removed test command

--- bot/categories.py
+++ bot/categories.py
@@ -103,10 +103,6 @@ class RoleplayCog(Cog, name='Roleplay'):
         )
         await ctx.send(message)
 
-    @command()
-    async def register_user(self, ctx, user_id):
-        await ctx.bot.confirm_user(int(user_id))
-
 
 class MiscellaneousCog(Cog, name='Miscellaneous'):
     """
