b6198e051b3d5d60e36ee027f6738ed0dddc0faa
LeCuay <suso.becerra98@gmail.com>
2020-03-31 14:17:50 +0200

    New check

--- runtests.py
+++ runtests.py
@@ -4,6 +4,7 @@ import argparse
 import subprocess
 import sys
 
+import django
 import pytest
 
 parser = argparse.ArgumentParser(description='Run Tests and Linting.')
@@ -44,12 +45,36 @@ def isort_main(args):
     return ret
 
 
-parser.add_argument('--no-lint', action='store_true',
-                    help='Indicates to no run lintings.')
-parser.add_argument('--lint-only', action='store_true',
-                    help='Runs only linting tests.')
+def django_check():
+    django.setup()
+    print('Checking Django')
+    ret = subprocess.call('{} manage.py check'.format(sys.executable))
+
+    if ret:
+        print('Django check failed.')
+    else:
+        print('Django check passed.')
+
+    return ret
+
+
+parser.add_argument('--no-lint', action='store_true', help='Indicates to not run lintings.')
+parser.add_argument('--lint-only', action='store_true', help='Runs only linting tests.')
 parser.add_argument('--fast', action='store_true', help='Run pytest quiet.')
 parser.add_argument('--coverage', action='store_true', help='Run coverage.')
+parser.add_argument('--no-check', action='store_true', help='Indicates to not run django check.')
+
+
+def run(_pytest, flake8, isort, _django):
+    if _pytest:
+        exit_on_failure(pytest.main(pytest_args))
+    if flake8:
+        exit_on_failure(flake8_main(FLAKE8_ARGS))
+    if isort:
+        exit_on_failure(isort_main(ISORT_ARGS))
+    if _django:
+        exit_on_failure(django_check())
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -57,12 +82,15 @@ if __name__ == '__main__':
     run_pytest = True
     run_flake8 = True
     run_isort = True
+    run_django_check = True
 
     if args.lint_only:
         run_pytest = False
     if args.no_lint:
         run_flake8 = False
         run_isort = False
+    if args.no_check:
+        run_django_check = False
     if args.fast:
         pytest_args = PYTEST_ARGS['fast']
     else:
@@ -70,9 +98,4 @@ if __name__ == '__main__':
     if args.coverage:
         pytest_args.extend(COVERAGE_ARGS)
 
-    if run_pytest:
-        exit_on_failure(pytest.main(pytest_args))
-    if run_flake8:
-        exit_on_failure(flake8_main(FLAKE8_ARGS))
-    if run_isort:
-        exit_on_failure(isort_main(ISORT_ARGS))
+    run(run_pytest, run_flake8, run_isort, run_django_check)
