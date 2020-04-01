ea9f193abc7249789e3199cb34d53b6ecd7b2c07
LeCuay <suso.becerra98@gmail.com>
2020-04-01 13:02:59 +0200

    Passing tests

--- tests/core/test_consumers.py
+++ tests/core/test_consumers.py
@@ -1,3 +1,4 @@
+import faker
 import pytest
 from channels.routing import URLRouter
 from channels.testing import WebsocketCommunicator
@@ -10,18 +11,39 @@ application = URLRouter([
     path('testws/', WebsocketCommunicator),
 ])
 
+fake = faker.Faker()
+
 
 @pytest.mark.asyncio
 async def test_websocket_sends_error():
     communicator = WebsocketCommunicator(HandlerJsonWebsocketConsumer, 'testws/')
     data = {
-        'dump': 'dump'
+        'dump': fake.word()
     }
+    await communicator.send_json_to(data)
+
     error_response = {
         'error': _('No type given') + '.'
     }
+    response = await communicator.receive_json_from()
+    assert response == error_response, 'Incorrect response.'
+
+    # Disconnect WebSocket to avoid pending tasks
+    await communicator.disconnect()
+
+
+@pytest.mark.asyncio
+async def test_websocket_handler_inexistent_function():
+    communicator = WebsocketCommunicator(HandlerJsonWebsocketConsumer, 'testws/')
+    data = {
+        'type': fake.word()
+    }
     await communicator.send_json_to(data)
     response = await communicator.receive_json_from()
+
+    error_response = {
+        'error': _('Inexistent type') + '.'
+    }
     assert response == error_response, 'Incorrect response.'
 
     # Disconnect WebSocket to avoid pending tasks
