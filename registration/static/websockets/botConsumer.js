f32a0f1fdc669e518b161f0bc31ca3ea99d780b8
LeCuay <suso.becerra98@gmail.com>
2020-04-01 14:24:13 +0200

    Added javascript translation

--- registration/static/websockets/botConsumer.js
+++ registration/static/websockets/botConsumer.js
@@ -1,17 +1,30 @@
-var debug = document.currentScript.getAttribute('debug');
-var ws_prefix = debug === 'True' ? 'ws://' : 'wss://'
-var url = ws_prefix + window.location.host + document.currentScript.getAttribute('consumer-url');
+let debug = document.currentScript.getAttribute('debug');
+let ws_prefix = debug === 'True' ? 'ws://' : 'wss://'
+let url = ws_prefix + window.location.host + document.currentScript.getAttribute('consumer-url');
 
-var socket = new WebSocket(url);
+let socket = new WebSocket(url);
+let btn_discord = $('#btn_discord_invite');
+let btn_discord_text = btn_discord.val();
+let input = $('#id_discord_id');
 
-$('#btn_discord_invite').click(function (e) {
-    var input = $('#id_discord_id').val();
-    data = JSON.stringify({
-        type: 'check_user',
-        discord_id: input
-    });
+btn_discord.click(function (e) {
+    var inputValue = input.val();
 
-    socket.send(data);
+    if (inputValue != "" && inputValue != undefined) {
+        data = JSON.stringify({
+            type: 'check_user',
+            discord_id: inputValue
+        });
+
+
+        try {
+            socket.send(data);
+            // Adding animation
+            btn_discord.val('. . . ');
+        } catch (e) {
+            console.error(e);
+        }
+    }
 });
 
 socket.onmessage = function (e) {
@@ -21,10 +34,10 @@ socket.onmessage = function (e) {
         console.error(data.error);
     }
 
-    if (data.exists) {
-        console.log('User exists!');
-    } else {
-        console.log('User does not exist.');
+    if (data.exists === true) {
+        btn_discord.val(gettext('Found') + '! ✓');
+    } else if (data.exists === false) {
+        btn_discord.val(gettext('User not found') + '! ✗');
     }
 };
 
