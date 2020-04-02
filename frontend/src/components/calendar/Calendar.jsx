7624ffe96897d99863fd7522cbc1d4d6ca4b883e
LeCuay <33956285+LeCuay@users.noreply.github.com>
2020-04-02 18:54:14 +0200

    OAR-0024 - Implement Calendar
    Using FullCalendar and React we implemented a modular Calendar.
    
    * Added
    * Added Jest (Test)
    * Modified Travis to accept and execute React Tests

---
+++ frontend/src/components/calendar/Calendar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+import './main.scss';
+
+export class Calendar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            events: []
+        }
+    }
+
+    render() {
+        return (
+            <div data-testid="calendar">
+                <FullCalendar
+                  defaultView="dayGridMonth"
+                  plugins={[ dayGridPlugin, interactionPlugin ]}
+                  events={this.state.events} />
+            </div>
+        )
+    }
+}
+
+export default Calendar;
