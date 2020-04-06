345bf2b05e709e78274fdc15838d5b779d912e10
LeCuay <suso.becerra98@gmail.com>
2020-04-06 11:55:44 +0200

    Code splitting!

--- frontend/src/renderUserCheckButton.js
+++ frontend/src/renderUserCheckButton.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Calendar from "./components/Calendar/Calendar";
 import UserCheckButton from "./components/buttons/discord/UserCheckButton/UserCheckButton";
 import JQuery from "jquery";
-
-let calendar = document.getElementById("oarCalendar");
-if (calendar != null && calendar != undefined) {
-	ReactDOM.render(<Calendar />, calendar);
-}
 
 let checkUserButton = document.getElementById("discord_check_user");
 if (checkUserButton != null && checkUserButton != undefined) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,42 @@
+const path = require("path");
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: [/\.js$/, /\.jsx$/],
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-          }
-        ]
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // // Translates CSS into CommonJS
-          "css-loader",
-          // // Compiles Sass to CSS
-          "sass-loader"
-        ]
-      }
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.scss', '.sass']
-  }
+	entry: {
+		calendar: "./frontend/src/renderCalendar.js",
+		userCheckButton: "./frontend/src/renderUserCheckButton.js"
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "./frontend/static/frontend/dist/"),
+	},
+	devServer: {
+		contentBase: "dist",
+	},
+	module: {
+		rules: [
+			{
+				test: [/\.js$/, /\.jsx$/],
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "babel-loader",
+					},
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// // Translates CSS into CommonJS
+					"css-loader",
+					// // Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".jsx", ".scss", ".sass"],
+	},
 };
