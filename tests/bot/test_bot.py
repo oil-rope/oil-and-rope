05fe884bddfb7a45845304a2a5f63bd2be0dad18
LeCuay <suso.becerra98@gmail.com>
2020-04-09 13:50:48 +0200

    New tests passing

--- tests/bot/test_bot.py
+++ tests/bot/test_bot.py
@@ -1,4 +1,7 @@
+from unittest.mock import patch
+
 from django.test import TestCase
+from faker import Faker
 
 from bot.bot import OilAndRopeBot
 from bot.exceptions import HelpfulError
@@ -10,10 +13,31 @@ class TestOilAndRopeBot(TestCase):
     """
 
     def setUp(self):
+        self.faker = Faker()
         self.bot_class = OilAndRopeBot
+        self.env_variables = {
+            'BOT_COMMAND_PREFIX': self.faker.word(),
+            'BOT_TOKEN': self.faker.password()
+        }
 
-    def test_instance_without_command_prefix(self):
+    def test_init_ok(self):
+        with patch.dict('os.environ', self.env_variables):
+            bot = self.bot_class()
+            self.assertEqual(self.env_variables['BOT_COMMAND_PREFIX'], bot.command_prefix)
+            self.assertEqual(self.env_variables['BOT_TOKEN'], bot.token)
+
+    def test_instance_without_bot_token_ko(self):
+        msg = 'Use .env file or set up BOT_TOKEN environment variables.'
+        key = 'BOT_COMMAND_PREFIX'
+        with patch.dict('os.environ', {key: self.env_variables[key]}):
+            with self.assertRaises(HelpfulError) as ex:
+                self.bot_class()
+        self.assertIn(msg, str(ex.exception))
+
+    def test_instance_without_command_prefix_ko(self):
         msg = 'Use .env file or set up BOT_COMMAND_PREFIX environment variables.'
-        with self.assertRaises(HelpfulError) as ex:
-            self.bot_class()
-            self.assertIn(msg, ex.message)
+        key = 'BOT_TOKEN'
+        with patch.dict('os.environ', {key: self.env_variables[key]}):
+            with self.assertRaises(HelpfulError) as ex:
+                self.bot_class()
+        self.assertIn(msg, str(ex.exception))
