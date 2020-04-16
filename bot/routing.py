addb64fd785820fb3fa3fadd82c931cbf8601f28
LeCuay <suso.becerra98@gmail.com>
2020-04-16 19:46:24 +0200

    Little fixes on routing and understanding WbSockets

--- bot/routing.py
+++ bot/routing.py
@@ -1,7 +1,11 @@
-from django.urls import re_path
+from django.urls import re_path, include
 
 from . import consumers
 
+BOT_PATTERNS = [
+    re_path(r'register/$', consumers.BotConsumer, name='ws_bot_register')
+]
+
 urlpatterns = [
-    re_path(r'ws/register/$', consumers.BotConsumer, name='ws_bot_register')
+    re_path(r'^ws/', include(BOT_PATTERNS))
 ]
