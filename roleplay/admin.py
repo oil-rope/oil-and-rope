372620319dab951bb9acd9e7e620ccd25bc6056e
LeCuay <suso.becerra98@gmail.com>
2020-05-08 11:38:00 +0200

    Added Django Admin

---
+++ roleplay/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
+
+from . import models
+
+
+@admin.register(models.Domain)
+class DomainAdmin(admin.ModelAdmin):
+    date_hierarchy = 'entry_created_at'
+    list_display = ('__str__', 'domain_type', 'entry_created_at', 'entry_updated_at')
+    list_display_links = ('__str__',)
+    list_filter = ('domain_type',)
+    readonly_fields = ('entry_created_at', 'entry_updated_at')
+    search_fields = ['name__icontains']
+
+
+@admin.register(models.Place)
+class PlaceAdmin(DraggableMPTTAdmin):
+    date_hierarchy = 'entry_created_at'
+    list_display = ('tree_actions', 'indented_title', '__str__', 'site_type', 'entry_created_at', 'entry_updated_at')
+    list_display_links = ('indented_title', '__str__')
+    list_filter = ('site_type',)
+    readonly_fields = ('entry_created_at', 'entry_updated_at')
+    search_fields = ['name__icontains']
