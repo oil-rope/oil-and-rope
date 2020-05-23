967e8c326bc91dbee745fddf9585393ad670c995
LeCuay <suso.becerra98@gmail.com>
2020-05-23 13:31:54 +0200

    Playing with Heroku

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
-web: bash ./heroku/web/start.sh
+# web: bash ./heroku/web/start.sh
+web: bin/start-nginx-solo
 bot: bash ./heroku/bot/start.sh
 daphne: bash ./heroku/websocket/start.sh
--- compose/channels/command.sh
+++ compose/channels/command.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 CYAN='\e[36m'
+GREEN='\e[32m'
 END='\e[0m'
+
+# Checking for existing socket
+if test -f "${DAPHNE_SOCKET}"; then
+  echo -e "${CYAN}Socket exists, deleting...${END}"
+  rm -f ${DAPHNE_SOCKET} && echo "${GREEN}Done!${END}"
+else
+  echo -e "${CYAN}Socket file doesn't exist.${END}"
+fi
 
 # Start server
 echo -e "${CYAN}Starting Daphne...${END}"
-daphne ${DAPHNE_ASGI_MODULE}:application -u ${DAPHNE_SOCKET} -p ${DAPHNE_PORT} --access-logfile ${DAPHNE_ACCESS_LOGFILE}
-
-# Listening to log
-tail -f -n 1 ${DAPHNE_ACCESS_LOGFILE}
+daphne ${DAPHNE_ASGI_MODULE}:application -u ${DAPHNE_SOCKET} -p ${DAPHNE_PORT} --access-log ${DAPHNE_ACCESS_LOGFILE}
