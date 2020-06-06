d76843fcb2ceeb2014cb9939b9d4f04f6027f64d
LeCuay <suso.becerra98@gmail.com>
2020-06-06 17:15:04 +0200

    D3 implementation for World List

--- roleplay/serializers.py
+++ roleplay/serializers.py
@@ -5,9 +5,14 @@ from .models import Place
 
 class PlaceSerializer(serializers.ModelSerializer):
 
-    user = serializers.HyperlinkedIdentityField(view_name='registration:user-detail')
-    owner = serializers.HyperlinkedIdentityField(view_name='registration:user-detail')
+    user = serializers.HyperlinkedRelatedField(view_name='registration:user-detail', read_only=True)
+    owner = serializers.HyperlinkedRelatedField(view_name='registration:user-detail', read_only=True)
+    children = serializers.SerializerMethodField()
 
     class Meta:
         model = Place
-        fields = ('name', 'description', 'site_type', 'parent_site', 'user', 'owner')
+        fields = ('id', 'name', 'description', 'site_type', 'parent_site', 'user', 'owner', 'children')
+
+    def get_children(self, obj):
+        children = obj.get_children()
+        return PlaceSerializer(children, context=self.context, many=True).data
---
+++ roleplay/static/roleplay/js/places/listWorlds.js
@@ -0,0 +1,64 @@
+const url = document.currentScript.getAttribute("place-api-url");
+let element = document.currentScript.getAttribute("source-view-element");
+
+$(() => {
+	element = document.querySelector(element);
+	const height = $(element).height();
+	const width = $(element).width();
+
+	// Selecting de SVG element for D3 and setting Height and Width
+	const svg = d3.select(element);
+	// Margin convetion
+	const margin = { top: 0, right: 150, bottom: 0, left: 70 };
+	const innerWidth = width - margin.left - margin.right;
+	const innerHeight = height - margin.bottom - margin.top;
+	const tree = d3.tree().size([innerHeight, innerWidth]);
+
+	const zoomG = svg.attr("width", width).attr("height", height).append("g");
+
+	const g = zoomG
+		.append("g")
+		.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+	svg.call(
+		d3.zoom().on("zoom", () => {
+			zoomG.attr("transform", d3.event.transform);
+		})
+	);
+
+	// GET from API
+	fetch(url)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			const root = d3.hierarchy(data);
+			const links = tree(root).links();
+			const linkPathGenerator = d3
+				.linkHorizontal()
+				.x((d) => d.y)
+				.y((d) => d.x);
+
+			g.selectAll("path")
+				.data(links)
+				.enter()
+				.append("path")
+				.attr("d", linkPathGenerator);
+
+			g.selectAll("text")
+				.data(root.descendants())
+				.enter()
+				.append("text")
+				.attr("x", (d) => d.y)
+				.attr("y", (d) => d.x)
+				.attr("dy", () => "0.32em")
+				// Tex align Middle for everything except leaf
+				.attr("text-anchor", (d) => (d.children ? "middle" : "start"))
+				// Font size from bigger to smaller
+				.attr("font-size", (d) => `${3.25 - d.depth}rem`)
+				// Text itself
+				.text((d) => {
+					return d.data.name;
+				});
+		});
+});
