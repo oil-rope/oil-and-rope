1ab4d27745b8fc65f776ac036b201b5655f8b92f
LeCuay <suso.becerra98@gmail.com>
2020-06-07 20:10:42 +0200

    Little fix for Codacy

--- heroku/web/start.sh
+++ heroku/web/start.sh
@@ -15,15 +15,15 @@ python manage.py collectstatic --noinput && echo -e "${GREEN}Done!${END}"
 # Checking for existing socket
 if test -f "${GUNICORN_SOCK}"; then
   echo -e "${CYAN}Socket exists, deleting...${END}"
-  rm -f ${GUNICORN_SOCK} && echo -e "${GREEN}Done!${END}"
+  rm -f "${GUNICORN_SOCK}" && echo -e "${GREEN}Done!${END}"
 else
   echo -e "${CYAN}Socket file doesn't exist. Creating...${END}"
-  touch ${GUNICORN_ACCESS_LOGFILE} && echo -e "${GREEN}Done!${END}"
+  touch "${GUNICORN_ACCESS_LOGFILE}" && echo -e "${GREEN}Done!${END}"
 fi
 
 # Start server
 echo -e "${CYAN}Starting server...${END}"
-gunicorn ${GUNICORN_WSGI_MODULE}:application -c python:${GUNICORN_CONFIG_FILE} && echo -e "${GREEN}Done!${END}"
+gunicorn "${GUNICORN_WSGI_MODULE}":application -c python:"${GUNICORN_CONFIG_FILE}" && echo -e "${GREEN}Done!${END}"
 
 # Listening to log
-tail -f -n 5 ${GUNICORN_ACCESS_LOGFILE}
+tail -f -n 5 "${GUNICORN_ACCESS_LOGFILE}"
