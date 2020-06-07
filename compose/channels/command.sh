b3d4646a194d86383225c02d77a4aced2be5d0e2
LeCuay <suso.becerra98@gmail.com>
2020-06-07 11:51:19 +0200

    Fixed some Codacy issues

--- compose/channels/command.sh
+++ compose/channels/command.sh
@@ -7,11 +7,11 @@ END='\e[0m'
 # Checking for existing socket
 if test -f "${DAPHNE_SOCKET}"; then
   echo -e "${CYAN}Socket exists, deleting...${END}"
-  rm -f ${DAPHNE_SOCKET} && echo "${GREEN}Done!${END}"
+  rm -f "${DAPHNE_SOCKET}" && echo "${GREEN}Done!${END}"
 else
   echo -e "${CYAN}Socket file doesn't exist.${END}"
 fi
 
 # Start server
 echo -e "${CYAN}Starting Daphne...${END}"
-daphne ${DAPHNE_ASGI_MODULE}:application -u ${DAPHNE_SOCKET} -p ${DAPHNE_PORT} --access-log ${DAPHNE_ACCESS_LOGFILE}
+daphne "${DAPHNE_ASGI_MODULE}":application -u "${DAPHNE_SOCKET}" -p "${DAPHNE_PORT}" --access-log "${DAPHNE_ACCESS_LOGFILE}"
--- compose/django/command.sh
+++ compose/django/command.sh
@@ -19,14 +19,14 @@ python manage.py collectstatic --noinput && echo -e "${GREEN}Done!${END}"
 # Checking for existing socket
 if test -f "${GUNICORN_SOCK}"; then
   echo -e "${CYAN}Socket exists, deleting...${END}"
-  rm -f ${GUNICORN_SOCK} && echo "${GREEN}Done!${END}"
+  rm -f "${GUNICORN_SOCK}" && echo "${GREEN}Done!${END}"
 else
   echo -e "${CYAN}Socket file doesn't exist.${END}"
 fi
 
 # Start server
 echo -e "${CYAN}Starting server...${END}"
-gunicorn ${GUNICORN_WSGI_MODULE}:application -c python:${GUNICORN_CONFIG_FILE} && echo -e "${GREEN}Done!${END}"
+gunicorn "${GUNICORN_WSGI_MODULE}":application -c python:"${GUNICORN_CONFIG_FILE}" && echo -e "${GREEN}Done!${END}"
 
 # Listening to log
-tail -f -n 5 ${GUNICORN_ACCESS_LOGFILE}
+tail -f -n 5 "${GUNICORN_ACCESS_LOGFILE}"
