72958b930cf18fb27e5f52e2d83d7639f749b493
LeCuay <suso.becerra98@gmail.com>
2020-06-22 18:14:54 +0200

    Tests

--- tests/bot/commands/roleplay/test_places.py
+++ tests/bot/commands/roleplay/test_places.py
@@ -6,11 +6,12 @@ from django.conf import settings
 from model_bakery import baker
 
 from bot.commands.roleplay import world_list
+from bot.commands.roleplay.places import world_delete
 from bot.models import DiscordUser
+from bot.utils import get_url_from
 from common.tools.sync import async_manager_func
 from common.tools.sync.models import async_get
 from tests.bot.helpers import mocks
-from bot.commands.roleplay.places import world_delete
 
 Place = apps.get_model('roleplay.Place')
 User = apps.get_model(settings.AUTH_USER_MODEL)
@@ -86,16 +87,38 @@ async def test_world_list_public_ok(mock_call, registered_author):
 @pytest.mark.django_db(transaction=True)
 @pytest.mark.asyncio
 @mock.patch('tests.bot.helpers.mocks.MemberMock.send')
-async def test_world_delete(mock_call, registered_author):
+async def test_world_delete_ok(mock_call, registered_author):
     author = registered_author
-    # We use 1 for convetion (to make check == 1 -> True), is indeed NOT RECOMMENDED
+    # We use 1 for convention (to make check == 1 -> True), is indeed NOT RECOMMENDED
     deleted_world = 1
-    bot = mocks.ClientMock(ignore_check_wait_for=True, wait_for_default=str(1))
+    bot = mocks.ClientMock(ignore_check_wait_for=True, wait_for_default=str(deleted_world))
     user = await async_get(User, discord_user__id=author.id)
-    worlds = baker.make(Place, 2, user=user, owner=user)
+    baker.make(Place, 2, user=user, owner=user)
     worlds = await async_manager_func(Place, 'own_places', user=user)
     deleted_world = worlds[1]
 
     await world_delete(author, bot)
     with pytest.raises(Place.DoesNotExist):
         await async_get(Place, pk=deleted_world.pk)
+
+    all_calls = mock_call.mock_calls
+    expected_delete_msg = f'Are you sure you want to delete {deleted_world}? [yes/no]'
+    assert mock.call(expected_delete_msg) in all_calls
+    del_url = await get_url_from('roleplay:world_delete', kwargs={'pk': deleted_world.pk})
+    info_msg = f'You can perform this action via web: {del_url}'
+    assert mock.call(info_msg) in all_calls
+    del_msg = f'{deleted_world} deleted.'
+    assert mock.call(del_msg) in all_calls
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+@mock.patch('tests.bot.helpers.mocks.MemberMock.send')
+async def test_world_delete_empty(mock_call, registered_author):
+    author = registered_author
+    bot = mocks.ClientMock(ignore_check_wait_for=True, wait_for_default=str(1))
+    await world_delete(author, bot)
+
+    all_calls = mock_call.mock_calls
+    empty_msg = 'Seems like you don\'t have any world yet.'
+    assert mock.call(empty_msg) in all_calls
