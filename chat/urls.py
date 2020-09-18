d81750aa576f90615e9346856d22ba4b8d82a037
LeCuay <suso.becerra98@gmail.com>
2020-09-18 23:16:53 +0100

    Removed test stuff

--- chat/urls.py
+++ chat/urls.py
@@ -1,15 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from . import views, viewsets
+from . import viewsets
+
+app_name = 'chat'
 
 router = routers.DefaultRouter()
 router.register(r'chat', viewset=viewsets.ChatViewSet, basename='chat')
 
-
-app_name = 'chat'
-
 urlpatterns = [
     path('api/', include((router.urls, 'api'))),
-    path('', views.BaseChatView.as_view(), name='main'),
 ]
