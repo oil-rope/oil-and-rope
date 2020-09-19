fdc92f5aed6e73caa3df502d4f6b2f9895a0e77e
LeCuay <suso.becerra98@gmail.com>
2020-09-19 12:16:31 +0100

    Tests viewsets

--- oilandrope/settings.py
+++ oilandrope/settings.py
@@ -264,7 +264,6 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
---
+++ tests/chat/test_viewsets.py
@@ -0,0 +1,69 @@
+from django.shortcuts import reverse
+from django.test import TestCase
+from model_bakery import baker
+
+from common.constants import models
+
+
+class TestChatViewSet(TestCase):
+    resolver_list = 'chat:api:chat-list'
+    resolver_detail = 'chat:api:chat-detail'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = baker.make(models.USER_MODEL, is_staff=False)
+        cls.staff_user = baker.make(models.USER_MODEL, is_staff=True)
+
+        cls.chat = baker.make(models.CHAT_MODEL)
+        cls.user_with_chat = baker.make(models.USER_MODEL, is_staff=False)
+        cls.chat.users.add(cls.user_with_chat)
+
+    def setUp(self):
+        self.list_url = reverse(self.resolver_list)
+        self.detail_url = reverse(self.resolver_detail, kwargs={'pk': self.chat.pk})
+
+    def test_access_list_anonymous_user_ko(self):
+        response = self.client.get(self.list_url)
+
+        self.assertEqual(403, response.status_code)
+
+    def test_access_list_user_not_in_chat_ko(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.list_url)
+
+        self.assertEqual(403, response.status_code)
+
+    def test_access_list_user_in_chat_ko(self):
+        self.client.force_login(self.user_with_chat)
+        response = self.client.get(self.list_url)
+
+        self.assertEqual(403, response.status_code)
+
+    def test_access_list_staff_user_ok(self):
+        self.client.force_login(self.staff_user)
+        response = self.client.get(self.list_url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_access_detail_anonymous_user_ko(self):
+        response = self.client.get(self.detail_url)
+
+        self.assertEqual(403, response.status_code)
+
+    def test_access_detail_user_not_in_chat_ko(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.detail_url)
+
+        self.assertEqual(403, response.status_code)
+
+    def test_access_detail_user_in_chat_ok(self):
+        self.client.force_login(self.user_with_chat)
+        response = self.client.get(self.detail_url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_access_detail_staff_user_ok(self):
+        self.client.force_login(self.staff_user)
+        response = self.client.get(self.detail_url)
+
+        self.assertEqual(200, response.status_code)
