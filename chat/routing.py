74d74b8ffd1d15c40974fff51bff133cb76bb28c
LeCuay <suso.becerra98@gmail.com>
2020-09-20 09:38:22 +0100

    Another gothix

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/connect/', consumers.ChatConsumer, name='connect'),
+    re_path(r'^ws/chat/connect/', consumers.ChatConsumer, name='connect'),
 ]
