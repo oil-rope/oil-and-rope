f6f7bd12f8a5ee86a4f43e2a17b54b4928a56c97
LeCuay <suso.becerra98@gmail.com>
2021-05-29 18:01:32 +0100

    Simplified Docker!

--- compose/django/command.sh
+++ compose/django/command.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 CYAN='\e[36m'
 GREEN='\e[32m'
 END='\e[0m'
-
-# Compile JavaScript
-echo -e "${CYAN}Installing NPM dependencies and building...${END}"
-npm install && npm run build && echo -e "${GREEN}Done!${END}"
 
 # Run migrations
 echo -e "${CYAN}Updating database...${END}"
@@ -16,17 +12,9 @@ python manage.py migrate && echo -e "${GREEN}Done!${END}"
 echo -e "${CYAN}Collecting statics...${END}"
 python manage.py collectstatic --noinput && echo -e "${GREEN}Done!${END}"
 
-# Checking for existing socket
-if test -f "${GUNICORN_SOCK}"; then
-  echo -e "${CYAN}Socket exists, deleting...${END}"
-  rm -f "${GUNICORN_SOCK}" && echo "${GREEN}Done!${END}"
-else
-  echo -e "${CYAN}Socket file doesn't exist.${END}"
-fi
-
 # Start server
 echo -e "${CYAN}Starting server...${END}"
-gunicorn "${GUNICORN_WSGI_MODULE}":application -c python:"${GUNICORN_CONFIG_FILE}" && echo -e "${GREEN}Done!${END}"
-
-# Listening to log
-tail -f -n 5 "${GUNICORN_ACCESS_LOGFILE}"
+gunicorn "${GUNICORN_WSGI_MODULE}":application \
+--access-logfile - --error-logfile - \
+--bind 0.0.0.0:8000 \
+--reload --workers 1
