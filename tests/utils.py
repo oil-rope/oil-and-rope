524b51d1b89117ffd1274543f2e5eca731488a70
LeCuay <suso.becerra98@gmail.com>
2022-03-18 19:32:27 +0000

    fix(test): added again `litecord` (#82)
    For better testing `Litecord` has been setted up again.
    
    Changes to be committed:
    	modified:   .github/workflows/django_dev.yml
    	modified:   .github/workflows/node_dev.yml
    	modified:   tests/bot/helpers/constants.py
    	modified:   tests/registration/test_forms.py
    	modified:   tests/utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,7 @@ import logging
 
 import requests
 
-from tests.bot.helpers.constants import LITECORD_API_URL
+from tests.bot.helpers.constants import LITECORD_API_URL, LITECORD_TOKEN
 
 LOGGER = logging.getLogger(__name__)
 
@@ -13,7 +13,14 @@ def check_litecord_connection() -> bool:
     """
 
     try:
-        response = requests.get(url=LITECORD_API_URL, timeout=5)
+        # NOTE: Since API Info seems to be dropped we check on bot user
+        response = requests.get(
+            url=f'{LITECORD_API_URL}users/@me',
+            timeout=5,
+            headers={
+                'Authorization': f'Bot {LITECORD_TOKEN}',
+            }
+        )
         return response.ok
     except requests.exceptions.ConnectionError as ex:
         LOGGER.exception(ex)
