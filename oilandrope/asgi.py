6f0cd6cae98ce5046fa85c4ed83d3c86864d4d49
LeCuay <suso.becerra98@gmail.com>
2022-03-28 13:13:23 +0100

    fix(env): environment variables now load
    Since there have been a lot of changes `daphne` and `ASGI` was kinda
    forgot.
    This quick fix should reintegrate everything to set it up correctly.
    
    Changes to be committed:
    	modified:   .env.example
    	modified:   oilandrope/asgi.py
    	modified:   oilandrope/gunicorn_conf.py
    	modified:   oilandrope/wsgi.py

--- oilandrope/asgi.py
+++ oilandrope/asgi.py
@@ -1,29 +1,25 @@
 from pathlib import Path
 
-import django
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import re_path
 
 from common.utils.env import load_env_file
-
-django.setup()
-
-from chat.consumers import ChatConsumer  # noqa: E402
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_FILE = BASE_DIR / '.env'
 load_env_file(ENV_FILE)
 
+django_asgi_app = get_asgi_application()
+
+from chat.consumers import ChatConsumer  # noqa: E402
+
 application = ProtocolTypeRouter({
-    'http': AsgiHandler(),
-    'websocket': AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter([
-                re_path(r'^ws/chat/$', ChatConsumer.as_asgi(), name='connect',),
-            ])
-        )
-    )
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            re_path(r'^ws/chat/$', ChatConsumer.as_asgi(), name='connect',),
+        ]),
+    ),
 })
--- oilandrope/gunicorn_conf.py
+++ oilandrope/gunicorn_conf.py
@@ -3,26 +3,43 @@
 import multiprocessing
 import os
 from distutils.util import strtobool as to_bool
+from pathlib import Path
 
-wsgi_module = os.getenv('GUNICORN_WSGI_MODULE', 'gunicorn_conf')
+from common.utils.env import load_env_file
+
+# If we load `.env` here we'll avoid loading it in each worker process
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE = BASE_DIR / '.env'
+load_env_file(ENV_FILE)
+
+# NOTE: We use `os.environ` to ensure that those variables are set
+
+wsgi_module = os.environ['GUNICORN_WSGI_MODULE']
 wsgi_app = f'{wsgi_module}:application'
 
-daemon = True
+daemon = bool(to_bool(os.environ['GUNICORN_DAEMON']))
 reload = True
-debug = bool(to_bool(os.getenv('DEBUG', 'False')))
-workers = multiprocessing.cpu_count() * 2 + 1
+debug = bool(to_bool(os.environ['DEBUG'], 'False'))
 
-accesslog = os.getenv('GUNICORN_ACCESS_LOGFILE', '-')
-errorlog = os.getenv('GUNICORN_ERROR_LOGFILE', '-')
+if 'GUNICORN_WORKERS' in os.environ:
+    workers = int(os.environ['GUNICORN_WORKERS'])
+else:
+    workers = multiprocessing.cpu_count() * 2 + 1
+
+accesslog = os.environ['GUNICORN_ACCESS_LOGFILE']
+errorlog = os.environ['GUNICORN_ERROR_LOGFILE']
 
 if debug:
     loglevel = 'debug'
 
-if 'GUNICORN_SOCK' in os.environ:
-    bind = 'unix:{socket}'.format(socket=os.getenv('GUNICORN_SOCK'))
+if 'GUNICORN_SOCK' in os.environ and os.environ['GUNICORN_SOCK']:
+    bind = 'unix:{socket}'.format(socket=os.environ['GUNICORN_SOCK'])
 else:
-    bind = '{ip}:{port}'.format(ip=os.getenv('GUNICORN_IP', '0.0.0.0'), port=os.getenv('GUNICORN_PORT', 8000))
+    bind = '{ip}:{port}'.format(
+        ip=os.environ['GUNICORN_IP'],
+        port=os.environ['GUNICORN_PORT'],
+    )
 
 if all(k in os.environ for k in ('GUNICORN_CERTFILE', 'GUNICORN_KEYFILE')):
-    certfile = os.getenv('GUNICORN_CERTFILE')
-    keyfile = os.getenv('GUNICORN_KEYFILE')
+    certfile = os.environ['GUNICORN_CERTFILE']
+    keyfile = os.environ['GUNICORN_KEYFILE']
