1c0b8155b4ad9a23f48c21a0e1a143ff7d47346e
LeCuay <suso.becerra98@gmail.com>
2022-03-31 09:52:31 +0100

    test(html): add test coverage for templates
    Now Django Templates (either `.html` or `.txt`) will be encapsulated in
    coverage to check if every condition is being triggered.
    
    BREAKING CHANGES: Added `DEBUG_TEMPLATE` variable and now is needed.
    
    Changes to be committed:
    	modified:   .env.example
    	modified:   .github/workflows/django.yml
    	modified:   bot/bot.py
    	deleted:    bot/templates/bot/templates/place_list.txt
    	deleted:    chat/templates/chat/index.html
    	modified:   common/utils/env.py
    	modified:   oilandrope/settings.py
    	modified:   poetry.lock
    	modified:   pyproject.toml
    	deleted:    roleplay/templates/roleplay/music/music_list.html
    	modified:   setup.cfg
    	modified:   tests/api/serializers/test_roleplay_serializer.py
    	modified:   tests/bot/test_bot.py
    	modified:   tests/chat/test_models.py
    	modified:   tests/registration/test_forms.py

--- tests/chat/test_models.py
+++ tests/chat/test_models.py
@@ -1,7 +1,12 @@
-from django.test import TestCase
+import unittest
+
+from django.test import TestCase, override_settings
 from model_bakery import baker
 
 from chat import models
+from tests.utils import check_litecord_connection
+
+from ..bot.helpers.constants import CHANNEL, LITECORD_API_URL, LITECORD_TOKEN
 
 
 class TestChat(TestCase):
@@ -9,12 +14,22 @@ class TestChat(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.instance = baker.make(cls.model)
+        cls.instance = baker.make_recipe('chat.chat')
 
     def test_str_ok(self):
         expected = f'{self.instance.name} ({self.instance.pk})'
 
         self.assertEqual(expected, str(self.instance))
+
+    def test_discord_chat_is_not_reached_when_no_discord_id(self):
+        self.assertIsNone(self.instance.discord_chat)
+
+    @unittest.skipIf(not check_litecord_connection(), 'Litecord seems to be unreachable.')
+    @override_settings(DISCORD_API_URL=LITECORD_API_URL, BOT_TOKEN=LITECORD_TOKEN)
+    def test_discord_chat_is_returned_when_discord_id_given_ok(self):
+        instance = baker.make_recipe('chat.chat', discord_id=CHANNEL)
+
+        self.assertIsNotNone(instance.discord_chat)
 
 
 class TestChatMessage(TestCase):
