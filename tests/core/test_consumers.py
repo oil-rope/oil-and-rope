e2a580a607ee3bb9c026cd820a7b33ca77c43f07
LeCuay <suso.becerra98@gmail.com>
2022-04-06 18:06:37 +0100

    test(consumer): consumer testing
    Add tests for `TypedConsumerMixin` and `HandlerJsonWebsocketConsumer`.
    
    Changes to be committed:
    	modified:   tests/core/test_consumers.py

--- tests/core/test_consumers.py
+++ tests/core/test_consumers.py
@@ -6,61 +6,75 @@ from core.consumers import HandlerJsonWebsocketConsumer
 from tests import fake
 
 
-@pytest.fixture(scope='function')
-def consumer():
-    class ConsumerClass(HandlerJsonWebsocketConsumer):
-        serializer_class = WebSocketMessageSerializer
+class TestTypedConsumerMixin:
+    @pytest.mark.asyncio
+    async def test_websocket_handler_not_given_serializer_class_ko(self):
+        class ConsumerClass(HandlerJsonWebsocketConsumer):
+            pass
 
-        async def test(self, content):
-            return await self.send_json(content)
+        communicator = WebsocketCommunicator(ConsumerClass.as_asgi(), '/ws/test/')
+        data = {
+            'type': fake.word()
+        }
+        await communicator.send_json_to(data)
 
-    return ConsumerClass
-
-
-@pytest.mark.asyncio
-async def test_websocket_sends_error(consumer):
-    communicator = WebsocketCommunicator(consumer.as_asgi(), '/ws/test/')
-    data = {
-        'dump': fake.word()
-    }
-    await communicator.send_json_to(data)
-
-    error_response = {
-        'type': 'error',
-        'content': {'message': 'Invalid data'},
-    }
-    response = await communicator.receive_json_from()
-    assert response == error_response
+        with pytest.raises(NotImplementedError):
+            await communicator.receive_json_from()
 
 
-@pytest.mark.asyncio
-async def test_websocket_handler_non_existent_function(consumer):
-    communicator = WebsocketCommunicator(consumer.as_asgi(), '/ws/test/')
-    data = {
-        'type': fake.word()
-    }
-    await communicator.send_json_to(data)
-    response = await communicator.receive_json_from()
+class TestHandlerJsonWebsocketConsumer:
+    @pytest.fixture(scope='class')
+    def consumer(self):
+        class ConsumerClass(HandlerJsonWebsocketConsumer):
+            serializer_class = WebSocketMessageSerializer
 
-    error_response = {
-        'type': 'error',
-        'content': {'message': 'Given type does not exist.'},
-    }
-    assert response == error_response
+            async def test(self, content):
+                return await self.send_json(content)
 
+        return ConsumerClass
 
-@pytest.mark.asyncio
-async def test_websocket_handler_existent_function(consumer):
-    communicator = WebsocketCommunicator(consumer.as_asgi(), '/ws/test/')
-    message = fake.word()
-    data = {
-        'type': 'test',
-        'content': {'message': message},
-    }
-    await communicator.send_json_to(data)
-    response = await communicator.receive_json_from()
+    @pytest.mark.asyncio
+    async def test_websocket_sends_error(self, consumer):
+        communicator = WebsocketCommunicator(consumer.as_asgi(), '/ws/test/')
+        data = {
+            'dump': fake.word()
+        }
+        await communicator.send_json_to(data)
 
-    assert response == {'content': {'message': message}}
+        error_response = {
+            'type': 'error',
+            'content': {'message': 'Invalid data'},
+        }
+        response = await communicator.receive_json_from()
+        assert response == error_response
+
+    @pytest.mark.asyncio
+    async def test_websocket_handler_non_existent_function(self, consumer):
+        communicator = WebsocketCommunicator(consumer.as_asgi(), '/ws/test/')
+        data = {
+            'type': fake.word()
+        }
+        await communicator.send_json_to(data)
+        response = await communicator.receive_json_from()
+
+        error_response = {
+            'type': 'error',
+            'content': {'message': 'Given type does not exist.'},
+        }
+        assert response == error_response
+
+    @pytest.mark.asyncio
+    async def test_websocket_handler_existent_function(self, consumer):
+        communicator = WebsocketCommunicator(consumer.as_asgi(), '/ws/test/')
+        message = fake.word()
+        data = {
+            'type': 'test',
+            'content': {'message': message},
+        }
+        await communicator.send_json_to(data)
+        response = await communicator.receive_json_from()
+
+        assert response == {'content': {'message': message}}
 
 
 @pytest.mark.asyncio
