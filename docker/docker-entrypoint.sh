6e804b56b8df6554f8aad4dfa9c0a50a13ca4c22
LeCuay <suso.becerra98@gmail.com>
2022-08-12 08:43:43 +0100

    ci(docker): update Dockerfile
    Updated Dockerfiles for all projects in order to get default environment
    variables.
    
    Changes to be committed:
    	modified:   .dockerignore
    	modified:   .github/workflows/django.yml
    	modified:   .gitignore
    	modified:   docker/3.10/Dockerfile.dev
    	modified:   docker/3.10/Dockerfile.prod
    	modified:   docker/3.9/Dockerfile.dev
    	modified:   docker/3.9/Dockerfile.prod
    	modified:   docker/docker-entrypoint.sh
    	modified:   oilandrope/settings.py

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,15 +3,17 @@
 set -Ee
 
 END='\033[0m'
-ERROR='\033[0;31m'
-SUCCESS='\033[0;32m'
-INFO='\033[0;36m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
 
-echo -e "${INFO}Checking project...${END}"
-poetry run ./manage.py check && echo -e "${GREEN}Project okay!${END}" || exit 1
+echo -e "${CYAN}Checking project...${END}"
+poetry run ./manage.py check && echo -e "${GREEN}Project okay!${END}" || \
+echo -e "${RED}Project is not okay${END}"
 
-echo -e "${INFO}Running migrations...${END}"
-poetry run ./manage.py migrate && echo -e "${GREEN}Database up to date!${END}" || exit 1
+echo -e "${CYAN}Running migrations...${END}"
+poetry run ./manage.py migrate && echo -e "${GREEN}Database up to date!${END}" || \
+echo -e "${RED}Couldn't update database. Can the project access?${END}"
 
-echo -e "${INFO}Starting project...${END}"
+echo -e "${CYAN}Starting project...${END}"
 poetry run gunicorn --config=python:oilandrope.gunicorn_conf
