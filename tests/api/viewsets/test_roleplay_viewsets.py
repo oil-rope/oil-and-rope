265611b63adfd746285bf99b7c9bab0863185425
LeCuay <suso.becerra98@gmail.com>
2022-08-16 13:00:16 +0100

    fix(dependencies): unnecessary `pyyaml`
    There was no need for `pyyaml`.
    
    Changes to be committed:
    	modified:   poetry.lock
    	modified:   tests/api/viewsets/test_roleplay_viewsets.py
    	modified:   tests/api/viewsets/test_roleplay_viewsets.py

--- tests/api/viewsets/test_roleplay_viewsets.py
+++ tests/api/viewsets/test_roleplay_viewsets.py
@@ -1,3 +1,4 @@
+from django.shortcuts import resolve_url
 from model_bakery import baker
 from rest_framework.status import HTTP_200_OK, HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND
 from rest_framework.test import APITestCase
@@ -40,3 +41,22 @@ class TestCampaignViewSet(APITestCase):
         response = self.client.get(url)
 
         self.assertEqual(HTTP_404_NOT_FOUND, response.status_code)
+
+
+class TestPlaceNestedViewSet(APITestCase):
+    resolver: str = 'api:roleplay:place-detail'
+    url: str
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.owner = baker.make_recipe('registration.user')
+        cls.parent_place = baker.make_recipe('roleplay.place', owner=cls.owner)
+        cls.instance = baker.prepare_recipe('roleplay.place')
+
+    def setUp(self) -> None:
+        self.url = resolve_url(self.resolver, pk=self.instance.pk)
+
+    def test_access_anonymous_ko(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(HTTP_403_FORBIDDEN, response.status_code)
