d26c0b2f87174af7eac7857ecdf8d36eb19fb1cc
LeCuay <suso.becerra98@gmail.com>
2022-09-05 13:30:45 +0100

    refactor(types): typing to models
    First approach to add typing on models, but either way for how Django
    works it's quite difficult to addapt it.
    
    Changes to be committed:
    	modified:   api/serializers/chat.py
    	modified:   common/baker_recipes.py
    	modified:   common/views/json.py
    	modified:   registration/models.py

--- api/serializers/chat.py
+++ api/serializers/chat.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING
+
 from django.apps import apps
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
@@ -7,9 +9,14 @@ from common.constants import models
 from .common import WebSocketMessageSerializer
 from .registration import SimpleUserSerializer
 
-User = get_user_model()
-ChatMessage = apps.get_model(models.CHAT_MESSAGE)
-Chat = apps.get_model(models.CHAT)
+if TYPE_CHECKING:
+    from chat.models import Chat as ChatModel
+    from chat.models import ChatMessage as ChatMessageModel
+    from registration.models import User as UserModel
+
+User: 'UserModel' = get_user_model()
+ChatMessage: 'ChatMessageModel' = apps.get_model(models.CHAT_MESSAGE)
+Chat: 'ChatModel' = apps.get_model(models.CHAT)
 
 
 class ChatMessageSerializer(serializers.ModelSerializer):
