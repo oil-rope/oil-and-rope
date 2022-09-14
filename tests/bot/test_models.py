c7ca908c1f4f93668512bd5c15dae647d146e589
LeCuay <suso.becerra98@gmail.com>
2022-09-14 17:43:12 +0100

    fix(OAR-88): iSort fix
    Changes to be committed:
    	modified:   tests/bot/test_models.py

--- tests/bot/test_models.py
+++ tests/bot/test_models.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 from django.test import TestCase, override_settings
 from faker import Faker
