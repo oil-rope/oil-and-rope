acb0c76982e38ad76c1ae2584d0d232c6a51aad9
LeCuay <suso.becerra98@gmail.com>
2023-01-04 20:01:22 +0000

    fix(OAR-113): clarification
    Added a help text to tell the user that they can upload images and the
    amount.
    Also removed the asterisk from the label so it fits the UX/UI.
    
    Changes to be committed:
    	modified:   common/forms/forms.py
    	modified:   roleplay/templates/roleplay/race/race_create.html

--- common/forms/forms.py
+++ common/forms/forms.py
@@ -8,6 +8,16 @@ from .mixins import FormCapitalizeMixin
 
 
 class ImageForm(FormCapitalizeMixin, forms.ModelForm):
+    """
+    This form is intended to be used alongside with `:class:~django.contrib.contenttypes.forms.BaseGenericInlineFormSet`
+    so the owner of the image will always be the user creating the instance that requires images.
+
+    Parameters
+    ----------
+    owner: `:class:~registration.models.User`
+        The user owner of this image.
+    """
+
     def __init__(self, owner: User, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.owner = owner
