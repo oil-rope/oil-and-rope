7f03bb8b21dc2ee793e83cfd08b8571df2910829
LeCuay <suso.becerra98@gmail.com>
2023-03-21 14:03:17 +0000

    fix(deploy): missing loading secrets on asgi
    We setup the loading secrets for `manage.py` but not for `asgi.py`
    neither `gunicorn_conf.py`.
    
    Changes to be committed:
    	modified:   oilandrope/asgi.py
    	modified:   oilandrope/gunicorn_conf.py

--- oilandrope/asgi.py
+++ oilandrope/asgi.py
@@ -6,11 +6,12 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 
-from common.utils.env import load_env_file
+from common.utils.env import load_env_file, load_secrets
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_FILE = BASE_DIR / '.env'
 load_env_file(ENV_FILE)
+load_secrets()
 
 django_asgi_app = get_asgi_application()
 
